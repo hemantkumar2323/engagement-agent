472d30ea552ca230684b172d36b0cf5bbcffb3d5
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-23 18:21:04 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CustomerEngagementAgent from './CustomerEngagementAgent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CustomerEngagementAgent />
+  </React.StrictMode>
+)
